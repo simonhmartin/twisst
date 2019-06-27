20f292e5b4852ae8d1d6a6dd3a03c622e3d2ecae
Simon Martin <shm45@cam.ac.uk>
2019-06-27 21:33:27 +0100

    fixed bug in reading trees from gzip files

--- plot_twisst.R
+++ plot_twisst.R
@@ -194,7 +194,7 @@ import.twisst <- function(weights_files, window_data_files=NULL, split_by_chrom=
     else{
         #otherwise we try to retrieve topologies from the (first) weights file
         n_topos = ncol(l$weights[[1]])
-        if (file_ext(weights_files[1]) == ".gz") cat="zcat" else cat="cat"
+        if (file_ext(weights_files[1]) == "gz") cat="zcat" else cat="cat"
         topos_text <- try(system(paste(cat, weights_files[[1]], "| head -n", n_topos), intern = T), TRUE)
         try(l$topos <- read.tree(text = topos_text))
         try(names(l$topos) <- sapply(names(l$topos), substring, 2))
